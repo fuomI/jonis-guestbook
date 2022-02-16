8717822f86b6ae5004b895d08ba1e7bba1d98fbd
Joni Jansson <joni.jansson@saunalahti.fi>
2022-02-16 14:53:37 +0200

    / -route (homepage) and /guestbook -route completed

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
 
+
 // THE ROUTES
 // **********
 
@@ -26,6 +27,60 @@ app.use(express.static('public'));
 app.get('/', function (req, res) {
     // Default route sends index.html as response
     res.sendFile(__dirname + "/index.html");
+});
+
+// The guestbook route
+app.get('/guestbook', function (req, res) {
+
+    // Load guestbook data
+    let data = require('./guestbookData.json');
+
+    // Get header and navbar
+    let guestbookContent = fs.readFileSync(__dirname + '/guestbook.html', err => {
+        if (err) throw err;
+    });
+
+    // Transform to header and navbar html to string
+    let contentString = guestbookContent.toString();
+
+    // Add table with data parsed into it
+
+    contentString += '<table class="pure-table" id="guestbook">';
+    contentString += '<thead>';
+    contentString += '<tr>';
+    contentString += '<th>#</th>';
+    contentString += '<th>Name</th>';
+    contentString += '<th>Message</th>';
+    contentString += '<th>Country</th>';
+    contentString += '</tr>';
+    contentString += '</thead>';
+    contentString += '<tbody>';
+
+    for (let i = 0; i < data.length; i++) {
+
+        contentString += '<tr>';
+        contentString += '<td>' + data[i].id + '</td>';
+        contentString += '<td>' + data[i].username + '</td>';
+        contentString += '<td>' + data[i].country + '</td>';
+        contentString += '<td>' + data[i].message + '</td>';
+        contentString += '</tr>';
+    }
+
+    contentString += '</tbody>';
+    contentString += '</table>';
+
+    // Add footer
+    let footer = fs.readFileSync(__dirname + '/footer.html', err => {
+        if (err) throw err;
+    });
+
+    let footerContent = footer.toString();
+
+    contentString += footerContent;
+
+    // Send header, navbar, and table with the data as response
+    res.send(contentString);
+
 });
 
 // The route 404
