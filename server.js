4cbf44bde5f957b8c7abd95b940be231f2e359b6
Joni Jansson <joni.jansson@saunalahti.fi>
2022-02-16 10:19:38 +0200

    created server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+// Import express, file system, node-fetch
+const express = require('express');
+const fs = require('fs');
+const nodeFetch = require('node-fetch');
+
+// Start express
+const app = express();
+
+// Declare PORT
+const PORT = process.env.PORT || 5000;
+
+// Add the body-parser module to handle form data
+const bodyParser = require('body-parser');
+
+// Enable body parser
+app.use(bodyParser.urlencoded({extended: true}));
+
